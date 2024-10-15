7d32beaf26f56b9581ba8f2c1fec1b83b4c3a760
davimdolabella <davimdolabella03@gmail.com>
2024-10-15 10:39:23 -0300

    testando novo estilo de menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 window.addEventListener("scroll", function(){
     let nav = document.querySelector("nav")
+    let menu = document.getElementById('menu')
+    let Etim = document.getElementById('Etim')
     nav.classList.toggle('sticky', window.scrollY > 0)
+    menu.classList.toggle('current', window.scrollY == 0)
+    Etim.classList.toggle('current', window.scrollY > 0)
 })
