029537a8ff402b673ec4cda8dd803c9940df0bf8
davimdolabella <davimdolabella03@gmail.com>
2024-10-15 17:54:03 -0300

    implementando aps e Hero

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-window.addEventListener("scroll", function(){
-    let nav = document.querySelector("nav")
-    let menu = document.getElementById('menu')
-    let Etim = document.getElementById('Etim')
-    nav.classList.toggle('sticky', window.scrollY > 0)
-    menu.classList.toggle('current', window.scrollY == 0)
-    Etim.classList.toggle('current', window.scrollY > 0)
-})
+const nav = document.querySelector("nav");
+const menu = document.getElementById('menu');
+const Etim = document.getElementById('Etim');
+const processorSvg = document.getElementById("processor-svg");
+const chartSvg = document.getElementById("chart-svg");
+const codeSvg = document.getElementById("code-svg");
+const heroContent = document.getElementsByClassName("hero-content-text");
+
+
+window.addEventListener("scroll", function() {
+    let value = window.scrollY;
+
+    nav.classList.toggle('sticky', window.scrollY > 0);
+    menu.classList.toggle('current', window.scrollY == 0);
+    Etim.classList.toggle('current', window.scrollY > 0);
+
+    processorSvg.style.top = `${300 + value * 0.4}px`;
+    chartSvg.style.top = `${500 + value * 0.5}px`;
+    codeSvg.style.top = `${600 + value * 0.7}px`;
+    for (let i = 0; i < 2; i++) {
+        heroContent[i].style.top = `${value * 0.5}px`;
+    }
+
+});
